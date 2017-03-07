a96bc096a90c5372f048372c0f7e4ed1882b465b
Jun Masuda <kibajun29@hotmail.co.jp>
2017-03-07 12:31:09 +0000

    add user model

---
+++ db/migrate/20170307123029_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :user_image
+      t.string :free_contents
+
+      t.timestamps null: false
+      t.index :email, unique: true # この行を追加
+    end
+  end
+end
