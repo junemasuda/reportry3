e6347957150eeb73710e13e04831521791a52e16
Jun Masuda <kibajun29@hotmail.co.jp>
2017-04-09 01:24:44 +0000

    ck

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,11 @@ module Reportry3
       g.helper false
       g.test_framework false
       
+      config.assets.precompile += Ckeditor.assets
+      config.assets.precompile += %w( ckeditor/* )
+      config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
+      
     end
   end
 end
